bc040518bb771efe8d31b00fa74c76baf2b34809
Ka-ma-sa <hyacinth8084@gmail.com>
2024-09-15 16:15:16 +0100

    update-js-weather-details

--- src/index.js
+++ src/index.js
@@ -2,9 +2,43 @@ function updateWeather(response) {
 	console.log(response);
 	let currentTemp = document.querySelector("#current-temp");
 	let cityName = document.querySelector("#city-name");
+	let weatherCondition = document.querySelector("#weather-condition");
+	let weatherHumidity = document.querySelector("#humidity");
+	let weatherWind = document.querySelector("#wind-speed");
+	let time = document.querySelector("#date-time");
+	let date = new Date(response.data.time * 1000);
 
-	cityName.innerHTML = response.data.city;
 	currentTemp.innerHTML = Math.round(response.data.temperature.current);
+	cityName.innerHTML = response.data.city;
+	weatherCondition.innerHTML = response.data.condition.description;
+	weatherHumidity.innerHTML = response.data.temperature.humidity;
+	weatherWind.innerHTML = response.data.wind.speed;
+	time.innerHTML = formateDate(date);
+}
+
+function formateDate(date) {
+	let mins = date.getMinutes();
+	let hours = date.getHours();
+	let days = [
+		"Sunday",
+		"Monday",
+		"Tuesday",
+		"Wednesday",
+		"Thursday",
+		"Friday",
+		"Saturday",
+	];
+	let day = days[date.getDay()];
+
+	if (mins < 10) {
+		mins = `0${mins}`;
+	}
+
+	if (hours < 10) {
+		hours = `0${hours}`;
+	}
+
+	return `${day}, ${hours}:${mins}`;
 }
 
 function searchCity(city) {
